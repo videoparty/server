25777ea8c290c62f9e1f0561901f6d6382ae4d02
Marnix <dev@primevideoparty.com>
2020-04-22 22:55:01 +0200

    Allow any origin to solve cors issues

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {SocketEvents} from "./websocket/socket-events";
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
-const ioserver = socketio(port);
+const ioserver = socketio(port,  { origins: '*:*'});
 new SocketEvents(ioserver);
 
 console.log('server up and running on port %s', port);
